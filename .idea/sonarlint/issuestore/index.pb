851fab4e70bd2599e8c6ab58f160015e5d4b4910
Jorgel <jorge_fant@hotmail.com>
2020-06-11 14:40:06 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+W
+'src/main/java/org/exemple/demo/App.java,d\c\dc9a395dba17f846cc1e537e735fe0350087a9f9
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/org/exemple/demo/App.java
@@ -0,0 +1,7 @@
+package org.exemple.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hola c'est moi ton appli en jar!");
+    }
+}
